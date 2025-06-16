8a29cd657eadd8446313488549e0825356acd8c1
yoo-sh-96 <tmdguq90051@gmail.com>
2025-06-16 17:32:17 +0900

    수정

--- Database/day_4/create.sql
+++ Database/day_4/create.sql
@@ -1,13 +1,28 @@
-INSERT INTO customers (name, age, city, country, customerNumber) VALUES ('seung-hyeop', 30, 'illsan', 'korea', 3333);
-INSERT INTO products (productsName, productCode, buyprice) VALUES ('Macbook', 's_33_3333', 2000000);
-INSERT INTO employees (employeeNumber, name, email) VALUES (29, 'seung-hyeop', 'tmdguq90051@gail.com');
-INSERT INTO offices (officeCode, country) VALUES (8, 'seoul');
-INSERT INTO oders (orderNumber, orderDate, customerNumber) VALUES (123, 2025-05-04, 3333);
-INSERT INTO orderdetails (orderNumber, productCode) VALUES (123, 's_33_3333');
-INSERT INTO payments (customerNumber, paymentDate) VALUES (3333, '2025-05-04');
-INSERT INTO productlines (productLine, textDescription) VALUES ('trains', 'oz_coding_school');
-INSERT INTO customers (name, age, city, country, customerNumber) VALUES ('parmer', 20, 'seoul', 'korea', 3334);
-INSERT INTO products (productsName, productCode, buyprice) VALUES ('Macbook-air', 's_33_3334', 1000000);
-INSERT INTO customers (name, age, city, country, customerNumber) VALUES ('seung-hyeop', 30, 'illsan', 'korea', 3333), ('seung-hyeop2', 30, 'illsan', 'korea', 3334);
-INSERT INTO products (productsName, productCode, buyprice) VALUES ('Macbook', 's_33_3333', 2000000), ('Macbook-air', 's_33_3334', 1000000);
-INSERT INTO employees (employeeNumber, name, email) VALUES (29, 'you', 'tmdguq90051@gail.com'), (30, 'youyou', 'tmdguq@gail.com');
+-- CREATE DATABASE airbnb;
+USE airbnb;
+-- Products 테이블
+CREATE TABLE Products (
+    productID INT AUTO_INCREMENT PRIMARY KEY,
+    productName VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    stockQuantity INT NOT NULL,
+    createDate TIMESTAMP
+);
+
+-- Customers 테이블
+CREATE TABLE Customers (
+    customerID INT AUTO_INCREMENT PRIMARY KEY,
+    customerName VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    address TEXT NOT NULL,
+    createDate TIMESTAMP
+);
+
+-- Orders 테이블
+CREATE TABLE Orders (
+    orderID INT AUTO_INCREMENT PRIMARY KEY,
+    customerID INT,
+    orderDate TIMESTAMP,
+    totalAmount DECIMAL(10, 2),
+    FOREIGN KEY (customerID) REFERENCES Customers(customerID)
+);
