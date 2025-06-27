f1636960db4354110c3c31dd06f06620b12e105f
yoo-sh-96 <tmdguq90051@gmail.com>
2025-06-27 16:17:02 +0900

    ✨ Django 9일차
    테스트코드 작성

--- django_oz/reviews/tests.py
+++ django_oz/reviews/tests.py
@@ -1,3 +1,38 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.urls import reverse
+from .models import Review 
+from users.models import User
+from feeds.models import Feed
 
-# Create your tests here.
+class ReviewAPITestCase(APITestCase):
+    def setUp(self):
+        # User Model
+        self.user = User.objects.create_user(username='testuser', password='password')
+        refresh = RefreshToken.for_user(self.user)
+        self.token = str(refresh.access_token)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token}') 
+
+        # Feed Model
+        self.feed = Feed.objects.create(user=self.user ,title = 'New Title')
+
+        # Review Model
+        self.review1 = Review.objects.create(content='Contnet 1', likes_num=0, user=self.user, feed=self.feed)
+        self.review2 = Review.objects.create(content='Contnet 2', likes_num=0, user=self.user, feed=self.feed)
+
+
+    def test_get_all_reviews(self):
+        url = reverse('reviews')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(Review.objects.count(), 2)
+
+    def test_get_review_detail(self):
+        url = reverse('review_detail', kwargs={'review_id':self.review1.id})
+
+        res=self.client.get(url)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data['content'], self.review1.content)
--- django_oz/users/urls.py
+++ django_oz/users/urls.py
@@ -1,9 +1,28 @@
 from django.urls import path
 from . import views
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
+
 
 urlpatterns = [
     path("", views.Users.as_view()),
     path("myinfo", views.MyInfo.as_view()),
-    path("getToken", obtain_auth_token)
+
+    # Authentication
+    path("getToken", obtain_auth_token), # DRF Token
+    path("login", views.Login.as_view()), # Django Session login
+    path("logout", views.Logout.as_view()), # Django Session logout
+
+    # JWT Authentication
+    path("login/jwt", views.JWTLogin.as_view()),
+    path("login/jwt/info", views.UserDetailView.as_view()),
+
+    # simple JWT Authentication
+    path("login/simpleJWT", TokenObtainPairView.as_view()),
+    path("login/simpleJWT/refresh", TokenRefreshView.as_view()),
+    path("login/simpleJWT/verify", TokenVerifyView.as_view())
 ]
